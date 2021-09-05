40ef8f4792d94953deaa3b2f7c74237598ead91b
Zdravko Cicin-Sain <zcsain@gmail.com>
2021-09-05 17:06:22 +0200

    Add basic check for Google API on Calendar page

--- src/components/pages/Calendar.jsx
+++ src/components/pages/Calendar.jsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Custom
 import CalendarViewSelector from "../elements/CalendarViewSelector";
 
 function Calendar() {
 	const [selectedViewType, setSelectedViewType] = useState(7);
+
+	useEffect(() => {
+		if (window.gapi.auth2) {
+			console.log("User is signed in to calendar");
+		} else {
+			console.log("User is not signed in to calendar");
+		}
+	});
 
 	return (
 		<React.Fragment>
