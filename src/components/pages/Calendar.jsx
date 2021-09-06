56140b4392439ab83822b8e71a62c815cc0f91e5
Zdravko Cicin-Sain <zcsain@gmail.com>
2021-09-06 13:28:40 +0200

    Calendar now redirects to SignIn page when user is not signed in, Calendar now renders chunkedCalendarData in lists

--- src/components/pages/Calendar.jsx
+++ src/components/pages/Calendar.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { DateTime } from "luxon";
+import { useHistory } from "react-router-dom";
 
 // Custom
 import CalendarViewSelector from "../elements/CalendarViewSelector";
 import AddEventDialog from "../elements/AddEventDialog";
 import dateToISO from "../../helpers/dateToISO";
 import chunkEventsV2 from "../../helpers/chunkEventsV2";
+import EventListV2 from "../elements/EventListV2";
+import Backdrop from "../elements/Backdrop";
 
 // Testing
 import EventList from "../elements/EventList";
 import eventList from "../../mockdata/eventList";
+import chunkedList7 from "../../mockdata/chunkedList7";
 
 // Material UI
 import { makeStyles } from "@material-ui/core/styles";
@@ -24,26 +28,29 @@ const useStyles = makeStyles((theme) => ({
 
 function Calendar() {
 	const classes = useStyles();
+	const history = useHistory();
 	const [selectedViewType, setSelectedViewType] = useState(7);
 	const [calendarData, setCalendarData] = useState([]);
 	const [chunkedCalendarData, setChunkedCalendarData] = useState([]);
 
 	// Get Events from calendar
 	const getEventList = (startDate, endDate, updateState) => {
-		window.gapi.client.calendar.events
-			.list({
-				calendarId: "primary",
-				timeMin: startDate,
-				timeMax: endDate,
-				showDeleted: false,
-				singleEvents: true,
-				orderBy: "startTime",
-			})
-			.then((response) => {
-				const events = response.result.items;
-				console.log("Events recieived");
-				updateState(events);
-			});
+		if (window.gapi.auth2) {
+			window.gapi.client.calendar.events
+				.list({
+					calendarId: "primary",
+					timeMin: startDate,
+					timeMax: endDate,
+					showDeleted: false,
+					singleEvents: true,
+					orderBy: "startTime",
+				})
+				.then((response) => {
+					const events = response.result.items;
+					console.log("Events recieived");
+					updateState(events);
+				});
+		}
 	};
 
 	// Fetch 1 day worth of events
@@ -69,6 +76,7 @@ function Calendar() {
 		getEventList(nowDate, days30LaterDate, setCalendarData);
 	};
 
+	// Handles event fetch base on selected view type
 	const handleEventFetch = () => {
 		if (selectedViewType === 1) {
 			fetch1();
@@ -89,7 +97,7 @@ function Calendar() {
 			console.log(
 				"User is not signed in to calendar, redirecting to sign in page"
 			);
-			// ADD REDIRECT TO SIGN IN PAGE HERE
+			history.push("/");
 		}
 
 		return () => {
@@ -114,25 +122,28 @@ function Calendar() {
 		console.log("Events chunked");
 	}, [calendarData]);
 
-	const testEvent = () => {
-		return (
-			<Grid item xs={12} sm={6} md={4} lg={3}>
-				<EventList events={eventList} viewType={selectedViewType} />
-			</Grid>
-		);
-	};
-
 	return (
 		<React.Fragment>
-			<p>Calendar</p>
-
-			<Container className={classes.container}>
-				<Grid container spacing={3} justifyContent="center">
-					{testEvent()}
-				</Grid>
-			</Container>
-			<CalendarViewSelector setSelectedViewType={setSelectedViewType} />
-			<AddEventDialog />
+			{chunkedCalendarData.length === 0 ? (
+				<Backdrop />
+			) : (
+				<React.Fragment>
+					{" "}
+					<Container className={classes.container}>
+						<Grid container spacing={3} justifyContent="center">
+							{chunkedCalendarData.map((chunk) => {
+								return (
+									<Grid key={chunk.date} item xs={12} sm={6} md={4} lg={3}>
+										<EventListV2 chunk={chunk} viewType={selectedViewType} />
+									</Grid>
+								);
+							})}
+						</Grid>
+					</Container>
+					<CalendarViewSelector setSelectedViewType={setSelectedViewType} />
+					<AddEventDialog />
+				</React.Fragment>
+			)}
 		</React.Fragment>
 	);
 }
